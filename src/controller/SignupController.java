8f870a310dfbace5e888db578358f441f568a98f
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-10-29 11:11:09 +0900

    signupController 수정

--- src/controller/SignupController.java
+++ src/controller/SignupController.java
@@ -1,30 +1,93 @@
 package controller;
 
+import domain.Member;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 public class SignupController {
 	
-	 @FXML
+	
+	  @FXML
+	    private Button btinsignup;
+
+	    @FXML
 	    private Label btnback;
 
 	    @FXML
-	    private BorderPane mainboardpane;
+	    private Label lblconfirm;
 
 	    @FXML
 	    private AnchorPane signuppane;
 
 	    @FXML
-	    void back(MouseEvent event) {
-	    	LoginController.getinstance().loadpage("login");
+	    private TextField txtemail;
+
+	    @FXML
+	    private TextField txtid;
+
+	    @FXML
+	    private TextField txtname;
+
+	    @FXML
+	    private PasswordField txtpassword;
+
+	    @FXML
+	    private PasswordField txtpasswordconfirm;
+
+	  
+	    @FXML
+	    void signup(ActionEvent event) {
+	    	//0 컨트롤 호출시 => fx:id 호출 [ 객체]
+	    	
+	    	//1. 유효성 검사
+	    		//1. 아이디 길이 체크
+	    		if(txtid.getText().length() < 4 || txtid.getText().length() > 13) {
+	    			lblconfirm.setText("아이디는 4 ~ 12 글자만 가능합니다."); //레이블 내용 변경
+	    			return; // 메소드 끝
+	    		}
+	    		//2. 패스워드 길이 체크
+	    		if(txtpassword.getText().length() < 4 || txtpassword.getText().length() > 8) {
+	    			lblconfirm.setText("비밀번호는 4 ~ 8 글자만 가능합니다."); // 레이블 내용 변경
+	    			return; // 메소드 끝
+	    		}
+	    		//3. 패스워드 동일 체크
+	    		if(!txtpassword.getText().equals(txtpasswordconfirm.getText())) {
+	    			//!:부정 [반대]
+	    			lblconfirm.setText("비밀번호가 동일하지 않습니다.");
+	    			return; // 메소드 끝
+	    		}
+	    		
+	    		//4. 이름길이 체크
+	    		if(txtname.getText().length() < 2) {
+	    			lblconfirm.setText("이름은 2글자 이상으로만 가능합니다.");
+	    			return;
+	    		}
+	    		//5. 이메일 길이 체크
+	    		if(txtemail.getText().length() < 5 && !txtemail.getText().contains("@")) {
+	    			lblconfirm.setText("이메일 형식으로 입력해주세요.");
+	    			return;
+	    		}
+	    		
+	    	//2. 중복체크
+	    	//3. 객체화
+	    	Member member = new Member(txtid.getText(), txtpassword.getText(), txtname.getText(), txtemail.getText());
+	    	
+	    	//4. 파일 혹은 DB처리
+	    	
+	    	//5. 메시지 창 띄우고 페이지 전환
+	    	lblconfirm.setText("가입해주셔서 감사합니다.");
 	    }
 
 	    @FXML
-	    void signup(MouseEvent event) {
-
+	    void back(MouseEvent event) {
+	    	LoginController.getinstance().loadpage("login");
 	    }
-
+	    
 }
---
+++ src/domain/Member.java
@@ -0,0 +1,94 @@
+package domain;
+
+public class Member {
+
+	//1. 필드
+	private String m_id;
+	private String m_pw;
+	private String m_name;
+	private String m_email;
+	private int m_point;
+	
+	//2. 생성자
+	
+		// 빈 생성자는 기본 세팅값
+	public Member() {}
+	
+		// 모든 필드를 받는 생성자 -> DB/File에서 가져올때 사용되는 생성자
+	public Member(String m_id, String m_pw, String m_name, String m_email, int m_point) {
+		super();
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_name = m_name;
+		this.m_email = m_email;
+		this.m_point = m_point;
+	}
+	
+	// 회원가입시 사용되는 생성자 [ 포인트 제외 ]
+	public Member(String m_id, String m_pw, String m_name, String m_email) {
+		super();
+		this.m_id = m_id;
+		this.m_pw = m_pw;
+		this.m_name = m_name;
+		this.m_email = m_email;
+		this.m_point = 1000; //포인트의 초기값
+	}
+
+	
+		
+	// 회원 수정시 사용되는 생성자
+	public Member(String m_name, String m_email, int m_point) {
+		super();
+		this.m_name = m_name;
+		this.m_email = m_email;
+		this.m_point = m_point;
+	}
+
+	
+
+	
+	//3. 메소드
+	
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getM_pw() {
+		return m_pw;
+	}
+
+	public void setM_pw(String m_pw) {
+		this.m_pw = m_pw;
+	}
+
+	public String getM_name() {
+		return m_name;
+	}
+
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+
+	public String getM_email() {
+		return m_email;
+	}
+
+	public void setM_email(String m_email) {
+		this.m_email = m_email;
+	}
+
+	public int getM_point() {
+		return m_point;
+	}
+
+	public void setM_point(int m_point) {
+		this.m_point = m_point;
+	}
+	
+	
+	
+}
