d7d0a48d9c7726cf13819010c986b6916ca4cf4d
ImaMature <ImaMature 88884623+ImaMature@users.noreply.github.com>
2021-11-03 11:28:42 +0900

    게시물 상세페이지에서 클릭시 조회수 증가, 게시물 삭제기능 구현

--- src/controller/BoardviewController.java
+++ src/controller/BoardviewController.java
@@ -1,14 +1,19 @@
 package controller;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import dao.BoardDao;
 import dao.MemberDao;
 import domain.Board;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -16,12 +21,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class BoardviewController implements Initializable{
+	Board board = BoardlistController.board; //board를 전역변수로 설정
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-		//개별 조회
-		Board board = BoardlistController.board;
+		//조회수 증감 메소드 호출
+			BoardDao.getBoardDao().viewupdate(board.getB_no());
+			
+			//개별 조회
 		txttitle.setText(board.getB_title());
 		txtcontents.setText(board.getB_contents());
 		lblwirter.setText("작성자 : " + board.getB_write() );
@@ -80,7 +88,24 @@ public class BoardviewController implements Initializable{
 
 	    @FXML
 	    void delete(ActionEvent event) {
-
+	    	Alert alert = new Alert(AlertType.CONFIRMATION);
+	    	alert.setHeaderText("해당 게시물을 삭제하시겠습니까?");
+	    	alert.setTitle("알림");
+	    	Optional<ButtonType> optional = alert.showAndWait();
+	    	if(optional.get() == ButtonType.OK) {
+	    		//삭제 진행
+	    		boolean result = BoardDao.getBoardDao().delete(board.getB_no());
+	    		if(result) {
+	    			alert.setHeaderText("게시물이 삭제되었습니다.");
+	    			alert.setTitle("알림");
+	    			alert.showAndWait();
+	    			MainpageController.getinstance().loadpage("boardlist");
+	    		}else {
+	    			alert.setHeaderText("관리자에게 문의");
+	    			alert.setTitle("알림");
+	    			alert.showAndWait();
+	    		}
+	    	}
 	    }
 
 	    @FXML
--- src/dao/BoardDao.java
+++ src/dao/BoardDao.java
@@ -58,7 +58,7 @@ public class BoardDao {
 			ObservableList<Board> boards = FXCollections.observableArrayList();
 			
 			//1. 조건 없이 모두 가져오기
-			String sql = "select*from board";
+			String sql = "select*from board order by b_no desc";
 			try {
 				preparedStatement = connection.prepareStatement(sql);
 				resultSet = preparedStatement.executeQuery();
@@ -80,9 +80,31 @@ public class BoardDao {
 		}
 	
 		//3-3) 게시물 삭제 메소드
+		public boolean delete (int b_no) {
+			String sql = "delete from board where b_no = ?";
+			try {
+				preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setInt(1, b_no);
+				preparedStatement.executeUpdate();
+				return true;
+			} catch (Exception e) {	}
+			return false;
+		}
+		
 		//3-4) 게시물 수정 메소드
 		//3-5) 게시물 개별조회 메소드
-		
+		//3-6) 게시물 조회수 증감 메소드
+		public boolean viewupdate(int b_no) {
+			String sql = "update board set b_view = b_view+1 where b_no=?";
+			
+			try {
+				preparedStatement = connection.prepareStatement(sql);
+				preparedStatement.setInt(1, b_no);
+				preparedStatement.executeUpdate();
+				return true;
+			}catch(Exception e) {}
+			return false;
+		}
 	
 	
 }
